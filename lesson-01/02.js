4dc98390a06500cdb32198ac298ac5fbbd60108b
mvkozak-fe <mvkozak.fe@gmail.com>
2024-05-24 10:40:26 +0300

    change output string

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,3 +1,3 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
-(number1%2 == 0) ? console.log(`${number1} :Это четное число`) : console.log(`${number1} :Это нечетное число`);
+(number1%2 == 0) ? console.log(`${number1} Это четное число`) : console.log(`${number1} Это нечетное число`);
