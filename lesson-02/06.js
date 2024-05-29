649a14b60b995f20b88a2a6821a5959ef566d3a0
mvkozak-fe <mvkozak.fe@gmail.com>
2024-05-29 11:36:59 +0300

    add task 6 lesson 2

---
+++ lesson-02/06.js
@@ -0,0 +1,12 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+let passportWithAddress2 = structuredClone(passportWithAddress);
+passportWithAddress2.address.city = 'Bobryisk';
+console.log(passportWithAddress);
+console.log(passportWithAddress2);
